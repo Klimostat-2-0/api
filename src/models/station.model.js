133228b9b97134870d3c9c36972f652937d8148e
Anzola Fabio <anzola.fabio@gmail.com>
2021-11-18 15:35:33 +0100

    mongoose station model

---
+++ src/models/station.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const { toJSON, paginate } = require('./plugins');
+
+const stationSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    roomNr: {
+      type: Number,
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// add plugin that converts mongoose to json
+stationSchema.plugin(toJSON);
+stationSchema.plugin(paginate);
+
+/**
+ * Check if name is taken
+ * @param {string} name - The stations name
+ * @param {ObjectId} [excludeStationId] - The id of the station to be excluded
+ * @returns {Promise<boolean>}
+ */
+ stationSchema.statics.isNameTaken = async function (name, excludeStationId) {
+  const station = await this.findOne({ name, _id: { $ne: excludeStationId } });
+  return !!station;
+};
+
+/**
+ * @typedef Station
+ */
+const Station = mongoose.model('Station', stationSchema);
+
+module.exports = Station;

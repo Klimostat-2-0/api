69d3c4b275ad7a5ebcca7d78f19c881b06da66b8
Anzola Fabio <anzola.fabio@gmail.com>
2021-11-18 17:54:58 +0100

    Measurement service

--- src/services/index.js
+++ src/services/index.js
@@ -3,3 +3,4 @@ module.exports.emailService = require('./email.service');
 module.exports.tokenService = require('./token.service');
 module.exports.userService = require('./user.service');
 module.exports.stationService = require('./station.service');
+module.exports.measurementService = require('./measurement.service');
---
+++ src/services/measurement.service.js
@@ -0,0 +1,44 @@
+const httpStatus = require('http-status');
+const { Measurement } = require('../models');
+const ApiError = require('../utils/ApiError');
+
+/**
+ * Create a Measurement
+ * @param {Object}  measurementBody
+ * @returns {Promise<Measurement>}
+ */
+const createMeasurement = async (measurementBody) => {
+	const measurement = await Measurement.create(measurementBody);
+	return measurement;
+};
+
+/**
+ * Query for Measurements
+ * @param {Object} filter - Mongo filter
+ * @param {Object} options - Query options
+ * @param {string} [options.sortBy] - Sort option in the format: sortField:(desc|asc)
+ * @param {number} [options.limit] - Maximum number of results per page (default = 10)
+ * @param {number} [options.page] - Current page (default = 1)
+ * @returns {Promise<QueryResult>}
+ */
+const queryMeasurements = async (filter, options) => {
+	const measurements = await Measurement.paginate(filter, options);
+	return measurements;
+};
+
+/**
+ * Get Measurement by id
+ * @param {ObjectId} id
+ * @returns {Promise<Measurement>}
+ */
+const getMeasurementById = async (id) => {
+	return Measurement.findById(id);
+};
+
+module.exports = {
+	createStation,
+	queryStations,
+	getStationById,
+	updateStationById,
+	deleteStationById,
+};

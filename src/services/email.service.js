8d27f2bf944a044627dcd0930db11dacd7eeb9f5
Anzola Fabio <anzola.fabio@gmail.com>
2021-11-30 14:21:38 +0100

    verify email

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -49,9 +49,12 @@ const sendVerificationEmail = async (to, token) => {
   const subject = 'Email Verification';
   // replace this url with the link to the email verification page of your front-end app
   const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
-  const text = `Dear user,
-To verify your email, click on this link: ${verificationEmailUrl}
-If you did not create an account, then ignore this email.`;
+  const text = `Dear Klimostat 2.0 user!
+To verify your email address, click on this link: 
+${verificationEmailUrl}
+
+If you did not create an account, then ignore this email.
+Your Klimostat 2.0 Team`;
   await sendEmail(to, subject, text);
 };
 
